44bda762b60a0770f8457991b214aac9ede9acc5
BazilSuhail <bazil1854@gmail.com>
2024-05-09 23:41:13 +0500

    Added Toastify Notifications !

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,15 +2,11 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth, fs } from "../Config/Config";
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
-
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 import "./Styles/loginsignup.css";
 
 const Login = () => {
-  const notify = () => toast("Wow so easy!");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userType, setUserType] = useState("donor");
@@ -23,16 +19,47 @@ const Login = () => {
       const snapshot = await collectionRef.where("email", "==", email).get();
 
       if (snapshot.empty) {
-        alert("Email not found in database. Please sign up.");
+        toast.error('Account not found', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark"
+        });
         return;
       }
 
       await auth.signInWithEmailAndPassword(email, password);
-      console.log("User logged in successfully!");
-      // alert("User logged in successfully!");
-      navigate(`/${userType}`);
+      toast.success('Login Successful !!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark"
+      });
+
+      setTimeout(() => {
+        navigate(`/${userType}`);
+      }, 3000);
+
     } catch (error) {
-      alert("Incorrect Credentials !!!");
+      //alert("Incorrect Credentials !!!");
+      toast.error('Incorrect Credentials !!!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark"
+      });
       console.error("Error logging in:", error.message);
     }
   };
@@ -45,39 +72,27 @@ const Login = () => {
   });
 
   return (
-    <div className="login-container">
+    <>
+      <ToastContainer />
+      <div className="login-container">
+        <div className="signup">Login</div>
+        <div className="signup-tagline">Login In !! {toDisplay}<Cursor cursorStyle='|' /></div>
+        <div className="text">  Don't Have an account? <Link className="link" to="/signup">Sign Up</Link>{" "}  </div>
 
-      <div className="signup">Login</div>
-      <div className="signup-tagline">Login In !! {toDisplay}<Cursor cursorStyle='|' /></div>
-      <div className="text">  Don't Have an account? <Link className="link" to="/signup">Sign Up</Link>{" "}  </div>
+        <form className="form-data" onSubmit={handleLogin}>
+          <div className="accType">How Would you like to Continue</div>
 
-      <form className="form-data" onSubmit={handleLogin}>
-        <div className="accType">How Would you like to Continue</div>
+          <select className="selection" value={userType} onChange={(e) => setUserType(e.target.value)}>
+            <option className="selection" value="donor">Donor</option>
+            <option className="selection" value="volunteer">Volunteer</option>
+          </select>
 
-        <select className="selection" value={userType} onChange={(e) => setUserType(e.target.value)}>
-          <option className="selection" value="donor">Donor</option>
-          <option className="selection" value="volunteer">Volunteer</option>
-        </select>
-
-        <input className="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <input className="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <button className="login-btn" onClick={notify} type="submit">Log In</button>
-
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-          transition:Bounce/>
-
-      </form>
-    </div>
+          <input className="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input className="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <button className="login-btn">Log In</button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -3,15 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { auth, fs } from "../Config/Config";
 import "./Styles/loginsignup.css";
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
-
   const [userType, setUserType] = useState("donor");
   const navigate = useNavigate();
-
 
   const handleSignUp = async (e) => {
     e.preventDefault();
@@ -26,12 +26,36 @@ const SignUp = () => {
         await fs.collection("volunteer").doc(user.uid).set(userData);
       }
 
-      console.log("User signed up successfully!");
-      alert("User signed up successfully!")
-      navigate("/login");
+      toast.success('Sign Up Successful !!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark"
+      });
+
+      //console.log("User signed up successfully!");
+      //navigate("/login");
+
+      setTimeout(() => {
+        navigate(`/login`);
+      }, 3000);
+
     } catch (error) {
       console.error("Error signing up:", error.message);
-      alert(error.message);
+      toast.error("Account Already in Use !!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark"
+      });
     }
   };
 
@@ -43,27 +67,27 @@ const SignUp = () => {
   });
 
   return (
-    <div className="login-container">
+    <>
+      <ToastContainer />
+      <div className="login-container">
+        <div className="signup">Sign Up</div> 
+        <div className="signup-tagline">Register Now !! {toDisplay}<Cursor cursorStyle='|' /></div>
+        <div className="text">Already have an account  <Link className="link" to="/login">Sign In</Link> </div>
 
-      <div className="signup">Sign Up</div> 
-      <div className="signup-tagline">Register Now !! {toDisplay}<Cursor cursorStyle='|' /></div>
-      <div className="text">Already have an account  <Link className="link" to="/login">Sign In</Link> </div>
+        <form className="form-data" onSubmit={handleSignUp}>
+          <div className="accType">How Would you like to Register</div>
+          <select className="selection" value={userType} onChange={(e) => setUserType(e.target.value)}>
+            <option className="selection" value="donor">Donor</option>
+            <option className="selection" value="volunteer">Volunteer</option>
+          </select>
 
-      <form className="form-data" onSubmit={handleSignUp}>
-        <div className="accType">How Would you like to Register</div>
-        <select className="selection" value={userType} onChange={(e) => setUserType(e.target.value)}>
-          <option className="selection" value="donor">Donor</option>
-          <option className="selection" value="volunteer">Volunteer</option>
-        </select>
-
-        <input className="display-name" type="text" placeholder="Display Name" value={displayName} onChange={(e) => setDisplayName(e.target.value)} required />
-        <input className="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <input className="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-        <button className="login-btn" type="submit">Sign Up</button>
-      </form>
-
-
-    </div>
+          <input className="display-name" type="text" placeholder="Display Name" value={displayName} onChange={(e) => setDisplayName(e.target.value)} required />
+          <input className="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input className="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+          <button className="login-btn" type="submit">Sign Up</button>
+        </form>
+      </div>
+    </>
   );
 };
 
